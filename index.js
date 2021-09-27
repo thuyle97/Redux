42f4b9080b4ee6516f98f6c68832bff4e2727a40
Thuy Le <phuongthuy9796@gmail.com>
2021-09-27 15:48:26 -0700

    Conbine Reducers

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
 
 const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
 
 function buyCake() {
   return {
@@ -10,26 +12,51 @@ function buyCake() {
   };
 }
 
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+  };
+}
+
 //(prevState, action) => newState
 
-const initialState = {
+const initialCakeState = {
   numofCakes: 10,
 };
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+  numofIceCreams: 20,
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case BUY_CAKE:
       return {
         ...state, // to copy the object before modify the property
         numofCakes: state.numofCakes - 1,
       };
-
     default:
       return state;
   }
 };
 
-const store = createStore(reducer); // the store holding the app states
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state, // to copy the object before modify the property
+        numofIceCreams: state.numofIceCreams - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducer); // the store holding the app states
 
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() =>
@@ -39,6 +66,8 @@ const unsubscribe = store.subscribe(() =>
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
-store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
 
 unsubscribe();
